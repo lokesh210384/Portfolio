576e9ec6c8ab36aa9e7dcc9bdf9ca257e922de3f
lokesh <lokesh210384@gmail.com>
2024-01-31 18:51:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var tablinks = document.getElementsByClassName("tablinks");
+var tabcontents = document.getElementsByClassName("tabcontents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("activelink");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("activetabs");
+    }
+    event.currentTarget.classList.add("activelink");
+    document.getElementById(tabname).classList.add("activetabs");
+}
+
